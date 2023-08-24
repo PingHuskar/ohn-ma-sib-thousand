7d1d73ed9791340c6c98069633f48d3b7463e0b6
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-08-25 00:59:32 +0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,109 @@
-import Head from 'next/head';
-
-import Layout from '@components/Layout';
-import Section from '@components/Section';
-import Container from '@components/Container';
 import Map from '@components/Map';
-import Button from '@components/Button';
-
+import Head from 'next/head'
 import styles from '@styles/Home.module.scss';
 
-const DEFAULT_CENTER = [38.907132, -77.036546]
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation'
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+     
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
 
 export default function Home() {
+  const size = useWindowSize();
+  const searchParams = useSearchParams()
+  const DEFAULT_CENTER = [Number(searchParams.get(`lat`)) || 13.744325,Number(searchParams.get(`lng`)) || 100.533507]
+  const zoom = 13
+  const r_km = Number(searchParams.get(`r`)) || 4
+  const [markers, setMarkers] = useState([])
+  useEffect(() => {
+    const url = `https://elaborate-alpaca-cd5c16.netlify.app/new/GenScript/pattaya/data.json`
+    axios.get(url)
+    .then(res => res.data)
+    .then(data => {
+      console.log(data)
+      setMarkers(data)
+    })
+    .catch(err => {
+      axios.get(`http://localhost:3000/api/portal`)
+      .then(res => res.data)
+      .then(data => {
+        console.log(data)
+        setMarkers(data)
+      })
+    })
+  },[])
+
   return (
-    <Layout>
-      <Head>
-        <title>Next.js Leaflet Starter</title>
-        <meta name="description" content="Create mapping apps with Next.js Leaflet Starter" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Section>
-        <Container>
-          <h1 className={styles.title}>
-            Next.js Leaflet Starter
-          </h1>
-
-          <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
-            {({ TileLayer, Marker, Popup }) => (
+    <>
+          <Head>
+            <title>{r_km} กิโลmaze</title>
+          </Head>
+          <Map className={styles.homeMap} 
+          width={size.width || 1200} 
+          height={size.height || 800} 
+          center={DEFAULT_CENTER} zoom={zoom}>
+            {({ TileLayer, Marker, Popup, Circle }) => (
               <>
                 <TileLayer
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
-                <Marker position={DEFAULT_CENTER}>
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
+
+                <Circle 
+                  center={DEFAULT_CENTER} 
+                  stroke={false} 
+                  fillColor="red" 
+                  radius={r_km*1000} 
+                />
+                {/* https://leafletjs.com/reference-1.3.4.html#path-weight */}
+
+                {markers.map((marker,index) => {
+                  console.log([marker.lat, marker.lng])
+                  if (!marker.image) return 
+                  return <Marker key={index} position={[marker.lat, marker.lng]} 
+                  icon={new L.icon({
+                    iconUrl: marker.image
+                    , iconSize: [42,42],
+                  })}>
+                    <Popup>
+                      {marker.name} <hr />
+                      {marker.address} <hr />
+                      {marker.lat},{marker.lng} <br />
+                    </Popup>
                 </Marker>
+                }) }
               </>
             )}
           </Map>
-
-          <p className={styles.description}>
-            <code className={styles.code}>yarn create next-app -e https://github.com/colbyfayock/next-leaflet-starter</code>
-          </p>
-
-          <p className={styles.view}>
-            <Button href="https://github.com/colbyfayock/next-leaflet-starter">Vew on GitHub</Button>
-          </p>
-        </Container>
-      </Section>
-    </Layout>
+    </>
   )
 }
